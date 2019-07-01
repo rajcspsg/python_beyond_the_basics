8b02bdf8cfacd20caa604cb146b19c88d37d6167
raj.cs.psg@gmail.com <raj.cs.psg@gmail.com>
2019-06-30 23:25:01 -0400

    completed namespace package

--- ch01_org_large_progs/reader/__init__.py
+++ ch01_org_large_progs/reader/__init__.py
@@ -1,3 +1,4 @@
 print('python found me!!!')
 
-from reader.reader import Reader
+from .reader import Reader
+# from reader.reader import Reader
---
+++ ch01_org_large_progs/reader/compressed/__init__.py
@@ -0,0 +1,4 @@
+from . import gzipped as gzip_opener
+from . import bzipped as bz2_opener
+
+__all__ = ['bz2_opener', 'gzip_opener']
